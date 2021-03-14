7a048f6b9fa922e17c40b7937dabf6b9b9c29e41
manoj surya <manoj@loadshare.net>
2021-03-14 22:07:35 +0530

    added test file
    Signed-off-by: manoj surya <manoj@loadshare.net>

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,8 @@
-const fullName = (firstName, lastName) => `Hey ${firstName} ${lastName}, this is your first test using Jest!`;
+const fullName = (firstName, lastName) => `${firstName} ${lastName}`;
 
 describe('Testing the tests setup', () => {
     it('Should return the full name', () => {
-        const result = `testing`;
+        const result = `Manoj suryawanshi`;
         expect(fullName("Manoj", "suryawanshi")).toBe(result);
     });
 });
